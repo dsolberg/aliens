d30f0c27f84b0e5cf12a0534a3e7143f9f33afb7
Darion Solberg <darion@gmail.com>
2018-03-29 03:45:31 -0400

    Adding more robust fight/death messages - fixed a couple bugs in movement and city destruction - fix #13

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func moveAliens() {
 	var randdir int
 
 	// Set direction map to map direction to random number
-	directions := map[int]string{1: "north", 2: "south", 3: "east", 4: "west"}
+	directions := map[int]string{0: "north", 1: "south", 2: "east", 3: "west"}
 
 	// Cycle through total aliens
 	for i := 0; i < total_aliens; i++ {
@@ -121,7 +121,7 @@ func fightAliens() {
 	// Loop for first alien to compare
 	for alien1 := 0; alien1 < total_aliens; alien1++ {
 
-		// Check to see if city is blown up 
+		// Check to see if city has already blown up
 		if city_map[aliens[alienName + strconv.Itoa(alien1)]] != nil {
 
 			// Loop for second alien to compare
@@ -135,22 +135,25 @@ func fightAliens() {
 
 						// Check if aliens are in the same city
 						if aliens[alienName + strconv.Itoa(alien1)] == aliens[alienName + strconv.Itoa(alien2)] {
-							fmt.Println("Match: " + strconv.Itoa(alien1) + " vs " + strconv.Itoa(alien2) + " in " + aliens[alienName + strconv.Itoa(alien1)] )
+							fmt.Println("Match: " + alienName + strconv.Itoa(alien1) + " vs " + alienName + strconv.Itoa(alien2) + " in " + aliens[alienName + strconv.Itoa(alien1)] + " - Result: all non-destroyed" )
 							
-							// Aliens kill eachother - pew pew
+							// Blow up city
+							delete(city_map,aliens[alienName + strconv.Itoa(alien1)])
+
+							// Aliens kill eachother in the smouldering city explosion - pew pew
 							aliens[alienName + strconv.Itoa(alien1)] = "dead"
 							aliens[alienName + strconv.Itoa(alien2)] = "dead"
 
-							// Blow up city
-							delete(city_map,aliens[alienName + strconv.Itoa(alien1)])
 						}
 					}
 				}	
 			}
+
 		} else {
 
-			// Alien in blown up city - assumed dead - collatoral damage
+			// Alien in blown up city - presumed dead - collatoral damage
 			aliens[alienName + strconv.Itoa(alien1)] = "dead"
+			fmt.Println("	- Poor " + alienName + strconv.Itoa(alien1) + " died in the crossfire")
 		}
 	}
 }
@@ -164,12 +167,12 @@ func main() {
     moveAliens()
 
     // Begin alien invasion
-    for tick := 0; tick < 1; tick++ {
+    for tick := 1; tick < 2; tick++ {
     	fmt.Println(tick)
 		moveAliens()
     	fightAliens()
     	time.Sleep(2 * time.Second)
     }
-    fmt.Println(aliens)
+    //fmt.Println(aliens)
 
 }
